0336cd2974001cc935243ac4be4b1e69a73f48f6
tubapolat <tubakalay@gmail.com>
2023-03-17 21:33:23 +0000

    remove redux

--- src/App.js
+++ src/App.js
@@ -2,27 +2,44 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import SingleCard from "./components/SingleCard";
 import { v4 as uuidv4 } from "uuid";
-import { useDispatch, useSelector } from "react-redux";
-import { setCards, setScore } from "./redux/cardSlice";
+
+const cardImages = [
+  { src: "/img/angel.jpg", matched: false },
+  //{ src: "/img/moulin.jpg", matched: false },
+  { src: "/img/children.jpg", matched: false },
+  // { src: "/img/arbore.jpg", matched: false },
+  { src: "/img/cassatt.jpg", matched: false },
+  //{ src: "/img/forest.jpg", matched: false },
+  // { src: "/img/gauguin.jpg", matched: false },
+  { src: "/img/gwen.jpg", matched: false },
+  // { src: "/img/kisling.jpg", matched: false },
+  { src: "/img/maurice.jpg", matched: false },
+  { src: "/img/monet.jpg", matched: false },
+  { src: "/img/morrisot.jpg", matched: false },
+  //{ src: "/img/players.jpg", matched: false },
+  { src: "/img/starry.jpg", matched: false },
+  { src: "/img/sun.jpg", matched: false },
+  { src: "/img/sunflowers.jpg", matched: false },
+  { src: "/img/suzanne.jpg", matched: false },
+  { src: "/img/van-gogh.jpg", matched: false },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
   const [choiceOne, setChoiceOne] = useState(null);
   const [choiceTwo, setChoiceTwo] = useState(null);
   const [disabled, setDisabled] = useState(false);
-
-  const cards = useSelector((state) => state.cards.items);
-  const score = useSelector((state) => state.cards.score);
-  const dispatch = useDispatch();
+  const [score, setScore] = useState(0);
 
   const shuffleCards = () => {
-    const shuffledCards = [...cards]
+    const shuffledCards = [...cardImages, ...cardImages]
       .sort(() => Math.random() - 0.5)
-      .map((card) => ({ ...card, id: uuidv4(), matched: false }));
+      .map((card) => ({ ...card, id: uuidv4() }));
 
     setChoiceOne(null);
     setChoiceTwo(null);
-    dispatch(setCards(shuffledCards));
+    setCards(shuffledCards);
     setTurns(0);
   };
 
@@ -39,23 +56,21 @@ function App() {
       setDisabled(true);
 
       if (choiceOne.src === choiceTwo.src) {
-        dispatch(
-          setCards([
-            ...cards.map((card) => {
-              if (card.src === choiceOne.src) {
-                return { ...card, matched: true };
-              } else {
-                return card;
-              }
-            }),
-          ])
-        );
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
         resetTurn();
       } else {
         setTimeout(() => resetTurn(), 1000);
       }
     }
-  }, [choiceOne, choiceTwo, dispatch]);
+  }, [choiceOne, choiceTwo]);
 
   const resetTurn = () => {
     setChoiceOne(null);
@@ -72,9 +87,9 @@ function App() {
     // const isGameOver =  cards.length !== 0 && cards.every((card) => card.matched === true);
     // if (isGameOver) {
     const userScore = 1200 - 15 * (turns * 2 - 24);
-    if (userScore > 0) dispatch(setScore(userScore));
+    if (userScore > 0) setScore(userScore);
     // }
-  }, [cards, turns, dispatch]);
+  }, [cards, turns]);
 
   return (
     <div className="App">
